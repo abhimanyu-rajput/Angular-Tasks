94959907b962d3fb843d416e2cf6c4916f841945
Abhi <rajput.abhimanyu2017.com>
2021-01-07 23:16:51 +0530

    fb test

--- src/app/social-login/facebook/facebook.component.ts
+++ src/app/social-login/facebook/facebook.component.ts
@@ -19,29 +19,30 @@ export class FacebookComponent implements OnInit {
     console.log(FB);
     
     await FB.init({
-      appId      : '513346442821314',
+      appId      : '1080651482398270',
       cookie     : true,                     // Enable cookies to allow the server to access the session.
       xfbml      : true,                     // Parse social plugins on this webpage.
       version    : 'v9.0'           // Use this Graph API version for this call.
     });
 
     FB.getLoginStatus((response) => {   // Called after the JS SDK has been initialized.
-      this.statusChangeCallback(response);        // Returns the login status.
+      // this.statusChangeCallback(response);        // Returns the login status.
+      console.log(response)
     });
     
 
   }
 
 
-  statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
-    console.log('statusChangeCallback');
-    console.log(response);                   // The current login status of the person.
-    if (response.status === 'connected') {   // Logged into your webpage and Facebook.
-      this.loginFb();  
-    } else {                                 // Not logged into your webpage or we are unable to tell.
-      console.log('Please log into this webpage.');
-    }
-  }
+  // statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
+  //   console.log('statusChangeCallback');
+  //   console.log(response);                   // The current login status of the person.
+  //   if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+  //     this.loginFb();  
+  //   } else {                                 // Not logged into your webpage or we are unable to tell.
+  //     console.log('Please log into this webpage.');
+  //   }
+  // }
 
   loginFb() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
     console.log('Welcome!  Fetching your information.... ');
