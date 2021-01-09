cf066dd40909776649482cab96f94ac216bf2aea
abhimanyu-rajput <abhimanyurajput07@gmail.com>
2021-01-09 17:52:39 +0530

    new changes rxjs

--- src/app/utils/rxjsops/rxjsops.component.ts
+++ src/app/utils/rxjsops/rxjsops.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
-import { fromEvent } from 'rxjs';
-import { debounceTime, exhaustMap, tap } from 'rxjs/operators';
+import { fromEvent, observable } from 'rxjs';
+import { debounceTime, exhaustMap, tap, map, distinctUntilChanged, switchMap } from 'rxjs/operators';
 import { HttpserviceService } from 'src/app/services/httpservice.service';
 import { TempdataService } from 'src/app/services/tempdata.service';
 
@@ -12,9 +12,10 @@ import { TempdataService } from 'src/app/services/tempdata.service';
 export class RxjsopsComponent implements OnInit, AfterViewInit {
 
   @ViewChild('btnGetUsrDtls') public btnGetUsrDtls:ElementRef;
+  @ViewChild('SearchByUserName') public SearchByUserName:ElementRef;
 
   public usersDetails:any;
-  // public searchUser:any;
+  public singleUser:any;
 
   constructor(private _httpserv:HttpserviceService, private _tds: TempdataService) { }
 
@@ -29,7 +30,7 @@ export class RxjsopsComponent implements OnInit, AfterViewInit {
     .pipe(
       tap(() => {
         this.btnGetUsrDtls.nativeElement.disabled = true;
-        this.btnGetUsrDtls.nativeElement.innerText = "Loading...";
+        this.btnGetUsrDtls.nativeElement.innerText = "Fetching Data...";
       }),
       debounceTime(3000),
       exhaustMap(() => this._httpserv.fatchData()),
@@ -42,6 +43,19 @@ export class RxjsopsComponent implements OnInit, AfterViewInit {
       console.log(res);
       this.usersDetails = res;
     });
+
+    // Get User details by username
+    fromEvent(this.SearchByUserName.nativeElement,'keyup')
+    .pipe(
+      map(res => {return res['target'].value}),
+      debounceTime(500),
+      distinctUntilChanged(),
+      switchMap((res:string) => this._httpserv.fatchUserData(res))
+    )
+    .subscribe(res => {
+      this.singleUser = res;
+      console.log(res);
+    })
 
   }
 
